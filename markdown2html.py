2ad1c20130ef6d867f6da4708edde563a33dcb61
SteveBanks0303 <111028784+SteveBanks0303@users.noreply.github.com>
2023-07-14 22:00:46 +0100

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -10,8 +10,7 @@ import hashlib
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
         exit(1)
 
     if not os.path.isfile(sys.argv[1]):
@@ -32,15 +31,13 @@ if __name__ == '__main__':
                 md5 = re.findall(r'\[\[.+?\]\]', line)
                 md5_inside = re.findall(r'\[\[(.+?)\]\]', line)
                 if md5:
-                    line = line.replace(md5[0], hashlib.md5(
-                        md5_inside[0].encode()).hexdigest())
+                    line = line.replace(md5[0], hashlib.md5(md5_inside[0].encode()).hexdigest())
 
                 # remove the letter C
                 remove_letter_c = re.findall(r'\(\(.+?\)\)', line)
                 remove_c_more = re.findall(r'\(\((.+?)\)\)', line)
                 if remove_letter_c:
-                    remove_c_more = ''.join(
-                        c for c in remove_c_more[0] if c not in 'Cc')
+                    remove_c_more = ''.join(c for c in remove_c_more[0] if c not in 'Cc')
                     line = line.replace(remove_letter_c[0], remove_c_more)
 
                 length = len(line)
@@ -50,39 +47,32 @@ if __name__ == '__main__':
                 unordered_num = length - len(unordered)
                 ordered = line.lstrip('*')
                 ordered_num = length - len(ordered)
+
                 # headings, lists
                 if 1 <= heading_num <= 6:
-                    line = '<h{}>'.format(
-                        heading_num) + headings.strip() + '</h{}>\n'.format(
-                        heading_num)
+                    line = '<h{}>'.format(heading_num) + headings.strip() + '</h{}>\n'.format(heading_num)
 
                 if unordered_num:
                     if not unordered_start:
                         html.write('<ul>\n')
                         unordered_start = True
                     line = '<li>' + unordered.strip() + '</li>\n'
-                if unordered_start and not unordered_num:
-                    html.write('</ul>\n')
-                    unordered_start = False
 
                 if ordered_num:
                     if not ordered_start:
                         html.write('<ol>\n')
                         ordered_start = True
                     line = '<li>' + ordered.strip() + '</li>\n'
-                if ordered_start and not ordered_num:
-                    html.write('</ol>\n')
-                    ordered_start = False
 
-                if not (heading_num or unordered_start or ordered_start):
+                if not (heading_num or unordered_num or ordered_num):
                     if not paragraph and length > 1:
                         html.write('<p>\n')
                         paragraph = True
-                    elif length > 1:
-                        html.write('<br/>\n')
-                    elif paragraph:
+                    elif length == 1:
                         html.write('</p>\n')
                         paragraph = False
+                    else:
+                        html.write('<br/>\n')
 
                 if length > 1:
                     html.write(line)
@@ -93,4 +83,5 @@ if __name__ == '__main__':
                 html.write('</ol>\n')
             if paragraph:
                 html.write('</p>\n')
-    exit (0)
+
+    exit(0)
